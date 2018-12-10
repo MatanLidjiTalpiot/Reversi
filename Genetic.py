ddbea11b474e0b37b4f388bbf9811049bd25c37a
matanLidjiTalpiot <matanlidji.39.talpiot@gmail.com>
2018-12-10 22:59:12 +0200

    wrote the basic UML - and started writing the evolve function

---
+++ Genetic.py
@@ -0,0 +1,36 @@
+import Game
+
+ALL_FUNCTIONS = []
+
+
+def play_game(h1, h2):
+    """
+    A function that plays a game between two heuristics
+    :param h1: heuristic number 1
+    :param h2: heuristic number 2
+    :return: the winning heuristic and the grades of each heuristic in the game
+    """
+    pass
+
+
+def evolve_N_time(h_list, n):
+    """
+    A function that gets a list if heuristics and evolves them n times
+    :param h_list: a list of heuristics
+    :param n: the nubmer of times to evolve them
+    :return: the list after the evolution
+    """
+    pass
+
+def evolve(heuristic, n, q):
+    """
+    A function that evolves a heuristic
+    :param heuristic: the heristic to evolve
+    :param n: the number of new heuristics to return
+    :param q: the quality of the evolution - the number of evolutions it goes trough
+    :return: a list of new evolved heuristics
+    """
+    evolved_list = []
+    if(q == 0):
+        return he
+    pass
