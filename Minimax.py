c98f6afc093706592becd72c35ad1f3b89f10766
Roey <roey8999>
2018-12-12 22:32:38 +0200

    Changed to most recent Minimax

--- Minimax.py
+++ Minimax.py
@@ -1,39 +1,47 @@
-<<<<<<< Updated upstream
-=======
 import Game
 import math
 import copy
+import numpy as np
 
-def minimax(game, depth, heuristic, maximizing_player, disk, path):
+
+def minimax(game, depth, initial_depth, heuristic, maximizing_player, disk, chosen_op):
     if (depth == 0) or (game.is_board_full()):
-        return [get_score(heuristic, game), path]
+        return [get_score(heuristic, game), chosen_op]
 
     options = game.get_legal_moves(disk)
     if maximizing_player:
         val = []
         val.append(-1 * math.inf)
-        val.append([])
-        print(game.board)
-        print("options: ", options)
-        print("current disk: ", disk)
+        val.append(None)
+        # print(game.board)
+        # print("options: ", options)
+        # print("current disk: ", disk)
         for op in options:
-            temp_game = game #todo maybe deepcopy
-            m = minimax(temp_game.do_move(disk, op), depth - 1, heuristic, False, -disk,
-                        [path, True])
+            temp_game = copy.deepcopy(game) # todo maybe deepcopy
+            temp_game.do_move(disk, op)
+            if depth == initial_depth:
+                chosen_op = op
+                print(op)
+            m = minimax(temp_game, depth - 1, initial_depth, heuristic, False,
+                        -disk, chosen_op)
             if m[0] > val[0]:
                 val = m
         return val
     else:
         val = []
         val.append(math.inf)
-        val.append([])
-        print(game.board)
-        print("options: ", options)
-        print("current disk: ", disk)
+        val.append(None)
+        # print(game.board)
+        # print("options: ", options)
+        # print("current disk: ", disk)
         for op in options:
-            temp_game = game #todo maybe deepcopy
-            m = minimax(temp_game.do_move(disk, op), depth - 1, heuristic, True, -disk,
-                        [path, False])
+            temp_game = copy.deepcopy(game)  # todo maybe deepcopy
+            temp_game.do_move(disk, op)
+            if depth == initial_depth:
+                chosen_op = op
+                print(options)
+            m = minimax(temp_game, depth - 1, initial_depth, heuristic, True, \
+                        -disk, chosen_op)
             if m[0] < val[0]:
                 val = m
         return val
@@ -54,7 +62,17 @@ def get_score(heuristic, game):
 
 
 game = Game.Game()
-heuristic = [[0.75, game.get_white_number], [0.1, game.get_black_number]]
-print(minimax(game, 1, heuristic, True, Game.BLACK, []))
+game.set_board(np.array([[0, 0, 0, 0, 0, -1, 0, 0],
+                      [0, 0, 0, 0, 1, -1, 0, 0],
+                      [1, 0, 1, -1, 1, -1, 0, 0],
+                      [1, 1, -1, -1, 1, -1, 0, 0],
+                      [1, 1, -1, -1, 1, -1, 0, 0],
+                      [0, -1, 1, -1, 0, -1, -1, 0],
+                      [0, 0, 1, 1, 1, 0, 1, 0],
+                      [0, 1, 0, 0, 0, -1, 0, 0]]).astype(int))
+heuristic = [[0, game.get_white_number], [1, game.get_black_number]]
 print(get_score(heuristic,game))
->>>>>>> Stashed changes
+m = (minimax(game, 2, 2, heuristic, True, Game.BLACK, None))
+print(m)
+game.do_move(Game.BLACK,m[1])
+print(get_score(heuristic, game))
