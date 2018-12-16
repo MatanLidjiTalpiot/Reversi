03f679c591b156df7de6e3f854f72fd313b9b9f4
matanLidjiTalpiot <matanlidji.39.talpiot@gmail.com>
2018-12-17 01:11:31 +0200

    found another state of the board thar crashes the code. and still need to find why it crashes

--- Sandbox.py
+++ Sandbox.py
@@ -3,15 +3,24 @@ import Minimax
 import Player
 import numpy as np
 game = Game.Game()
-game.set_board(np.array([[-1, -1, -1, -1, -1, -1, -1, -1],
-                [-1,  1, -1, -1,  1,  0, -1,  0],
-                [-1, -1, -1,  1,  1,  1,  0,  0],
-                [-1, -1,  1,  1,  1,  1,  1,  0],
-                [ 0,  0,  1,  1,  1,  1,  0,  0],
-                [ 0,  0,  1,  1,  1,  1,  1,  0],
-                [ 0,  0,  0,  1,  0,  0,  0,  0],
-                [ 0,  0,  1,  0,  0,  0,  0,  0]]))
-print("legal", game.get_legal_moves(1))
+# game.set_board(np.array([[-1, -1, -1, -1, -1, -1, -1, -1],
+#                 [-1,  1, -1, -1,  1,  0, -1,  0],
+#                 [-1, -1, -1,  1,  1,  1,  0,  0],
+#                 [-1, -1,  1,  1,  1,  1,  1,  0],
+#                 [ 0,  0,  1,  1,  1,  1,  0,  0],
+#                 [ 0,  0,  1,  1,  1,  1,  1,  0],
+#                 [ 0,  0,  0,  1,  0,  0,  0,  0],
+#                 [ 0,  0,  1,  0,  0,  0,  0,  0]]))
+#print("legal", game.get_legal_moves(1))
+
+# [[ 0  0  0  0  0  0  0  0]
+#  [ 0  0  0  0  0  0  0  0]
+#  [-1  1  1  1  0  0  1  0]
+#  [-1 -1  1  1  1  1  0  0]
+#  [-1 -1 -1  1 -1  1  0  0]
+#  [-1 -1  1 -1  1  1  1  0]
+#  [-1 -1 -1 -1 -1 -1 -1 -1]
+#  [-1 -1 -1 -1 -1 -1 -1 -1]]
 
 h = [[1, lambda game: game.get_black_number()], [-1, lambda game: game.get_white_number()]]
 player1 = Player.Player(heuristic=h, disk= 1, name = "computer")
