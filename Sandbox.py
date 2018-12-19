8cd764d77218301e58ea57b061eb19e925bcdf50
matanLidjiTalpiot <matanlidji.39.talpiot@gmail.com>
2018-12-19 14:54:08 +0200

    paltis modifications

--- Sandbox.py
+++ Sandbox.py
@@ -79,28 +79,30 @@ h4 = [[1, lambda game: game.get_black_number()],
 
 palti_n = np.log(10)/np.log(1.5)
 palti_A = 1/(40**palti_n)
-palti_black = [[1, lambda game: (game.get_black_number()*palti_A*np.power(game.get_number_of_turns(
+palti_black = [[10, lambda game: (game.get_black_number()*palti_A*np.power(game.get_number_of_turns(
 
 ),palti_n))],
-               [-1, lambda game: (game.get_white_number()*palti_A*np.power(game.get_number_of_turns(
+               [-10, lambda game: (game.get_white_number()*palti_A*np.power(
+                   game.get_number_of_turns(
 
 ),palti_n))],
-               [100,lambda game: game.get_num_of_cornors(1)],
-               [-1000, lambda game: game.get_num_of_cornors(-1)],
+               [1000,lambda game: game.get_num_of_cornors(1)],
+               [-10000, lambda game: game.get_num_of_cornors(-1)],
                [50, lambda game: game.get_num_of_sides(1)],
                [-200, lambda game: game.get_num_of_sides(-1)],
                [-500, lambda game: game.get_num_of_options_for_other(1)],
                [sys.maxsize / 128, lambda game: game.is_winner_score(1)]]
 
 
-palti_white = [[-1, lambda game: (game.get_black_number()*palti_A*np.power(game.get_number_of_turns(
+palti_white = [[-10, lambda game: (game.get_black_number()*palti_A*np.power(
+    game.get_number_of_turns(
 
 ),palti_n))],
-               [1, lambda game: (game.get_white_number()*palti_A*np.power(game.get_number_of_turns(
+               [10, lambda game: (game.get_white_number()*palti_A*np.power(game.get_number_of_turns(
 
 ),palti_n))],
-               [100,lambda game: game.get_num_of_cornors(-1)],
-               [-1000, lambda game: game.get_num_of_cornors(1)],
+               [1000,lambda game: game.get_num_of_cornors(-1)],
+               [-10000, lambda game: game.get_num_of_cornors(1)],
                [50, lambda game: game.get_num_of_sides(-1)],
                [-200, lambda game: game.get_num_of_sides(1)],
                [-500, lambda game: game.get_num_of_options_for_other(-1)],
@@ -112,6 +114,7 @@ palti_black = Player.Player(heuristic=palti_black, disk= 1, name = "palti black"
 palti_white = Player.Player(heuristic=palti_white, disk= 1, name = "palti white")
 h1_white= Player.Player(disk = -1, name = "h1 white",heuristic=h1_white)
 h1_black= Player.Player(disk = -1, name = "h1 black",heuristic=h1_black)
+yair = Player.Player(disk = -1, name = "yair", type = Player.Player.PlayerTypes.HUMAN)
 
 dic = {palti_black.name:0, palti_white.name:0, h1_black.name : 0, h1_white.name:0}
 for i in range (1):
@@ -125,7 +128,7 @@ for i in range (1):
     print("winner of the ", i, "game is: ", winner.name)
     game.reset_game()
     """
-    winner = Gui.play_game(game, palti_black, palti_white, to_print=False)
+    winner = Gui.play_game(game, yair, palti_white, to_print=False)
     dic[winner.name] += 1
     print("winner of the ", i, "game is: ", winner.name)
     print(dic)
