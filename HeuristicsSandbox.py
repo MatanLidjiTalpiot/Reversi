68a1cf76f0576ced3a6e770a1403d70295861faa
Roey <roey8999>
2019-01-03 18:53:09 +0200

    Merged conflicts

--- HeuristicsSandbox.py
+++ HeuristicsSandbox.py
@@ -1,6 +1,8 @@
 import numpy as np
 import sys
-
+import Game
+import Player
+import Gui
 h1_black = [[1, lambda game: game.get_black_number()],
             [-1, lambda game: game.get_white_number()],
             [100, lambda game: game.get_num_of_corners(1)],
@@ -57,10 +59,11 @@ palti_white_h = [
     [-200, lambda game: game.get_num_of_sides(1)],
     [-500, lambda game: game.get_num_of_options_for_other(-1)],
     [sys.maxsize / 128, lambda game: game.is_winner_score(-1)]]
-
 palti_black = Player.Player(heuristic=palti_black_h, disk=1, name="Computer")
 
-palti_white = Player.Player(heuristic=palti_white_h, disk=1, name="Computer")
+palti_white = Player.Player(heuristic=palti_white_h, disk=-1, name="Computer")
 p1_white = Player.Player(disk=-1, name="h1 white", heuristic=h1_white)
 p1_black = Player.Player(disk=-1, name="h1 black", heuristic=h1_black)
-eisner = Player.Player(disk=-1, name="Eisner", type=Player.Player.PlayerTypes.HUMAN)
+eisner = Player.Player(disk=1, name="Eisner", type=Player.Player.PlayerTypes.HUMAN)
+game = Game.Game(palti_white, eisner)
+Gui.play_game(game, to_print=True)
