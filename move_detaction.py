f4b23a8a40c5475ea00f4a7b720b56a50ed4826d
t8495050 <drori39talpiot@gmail.com>
2019-03-24 01:12:40 +0200

    stop the code until it is our turn again

--- move_detaction.py
+++ move_detaction.py
@@ -1,5 +1,6 @@
 from ImageProcessing import *
 take_grayscale_pic()
+import time
 sdThresh = 10
 x_min = 0
 x_max = 0
@@ -14,14 +15,9 @@ def cut_picture_2(img):
 
 
 def is_hand_in_the_field(refrence, pic):
-    refrence = cut_picture_2(refrence)
-    pic = cut_picture_2(pic)
     if distMap(pic, refrence) >= sdThresh:
         return True
     return False
-
-
-font = cv2.FONT_HERSHEY_SIMPLEX
 
 
 def distMap(frame1, frame2):
@@ -35,24 +31,85 @@ def distMap(frame1, frame2):
     return dist
 
 
-def num_of_circles():
+def num_of_circles(board_pic):
+    return len(filter_circles(find_circles(board_pic)))
 
-    circles = filter_circles(find_circles())
-    return
 
-def is_our_turn(num_of_circles):
-    gray_refrence = take_grayscale_pic()
+def continue_when_it_is_our_turn(last_num_of_circles):
+    ##take refrence picture
+    take_grayscale_pic()
     colored_refrence = take_color_pic()
+    #cut the refrence
+    refrence = cut_picture_2(colored_refrence)
+
+    ##take picture
     rval, colored_pic = cam.read()
-    is_hand = is_hand_in_the_field(colored_refrence,colored_pic)
+    #cut it
+    pic = cut_picture_2(pic)
+
+    #initial values
+    is_hand = is_hand_in_the_field(refrence,pic)
     our_turn = False
-    while
-    while is_hand is False:
-        rval, colored_pic = cam.read()
-        is_hand = is_hand_in_the_field(colored_refrence, colored_pic)
 
-    while is_hand is True:
-        rval, colored_pic = cam.read()
-        is_hand = is_hand_in_the_field(colored_refrence, colored_pic)
+    while our_turn is False:
+        print('not our turn yet')
+        #wait until there is a hand in the board
+        while is_hand is False:
+            rval, colored_pic = cam.read()
+            pic = cut_picture_2(pic)
+            is_hand = is_hand_in_the_field(colored_refrence, colored_pic)
+            time.sleep(0.1)
 
+        # wait until there is no hand in the board
+        while is_hand is True:
+            rval, colored_pic = cam.read()
+            pic = cut_picture_2(pic)
+            is_hand = is_hand_in_the_field(colored_refrence, colored_pic)
+            time.sleep(0.1)
+
+        #check
+        board_pic = cutPicture(colored_pic)
+        circles_N = num_of_circles(board_pic)
+        #if legal
+        if circles_N == last_num_of_circles + 1:
+            return True
+        elif circles_N == last_num_of_circles:
+            rval, colored_pic = cam.read()
+            pic = cut_picture_2(pic)
+            is_hand = is_hand_in_the_field(colored_refrence, colored_pic)
+            while is_hand is True:
+                rval, colored_pic = cam.read()
+                pic = cut_picture_2(pic)
+                is_hand = is_hand_in_the_field(colored_refrence, colored_pic)
+                time.sleep(0.1)
+
+            board_pic = cutPicture(colored_pic)
+            circles_N = num_of_circles(board_pic)
+            if circles_N == last_num_of_circles + 1:
+                return True
+            elif circles_N == last_num_of_circles:
+                continue
+
+        #if illegal we check again
+        else :
+            rval, colored_pic = cam.read()
+            pic = cut_picture_2(pic)
+            is_hand = is_hand_in_the_field(colored_refrence, colored_pic)
+            while is_hand is True:
+                rval, colored_pic = cam.read()
+                pic = cut_picture_2(pic)
+                is_hand = is_hand_in_the_field(colored_refrence, colored_pic)
+                time.sleep(0.1)
+
+            board_pic = cutPicture(colored_pic)
+            circles_N = num_of_circles(board_pic)
+            if circles_N == last_num_of_circles + 1:
+                return True
+            elif circles_N == last_num_of_circles:
+                our_turn = False
+            else:
+                print ('illegal move')
+                return True
+
+    return our_turn
 
